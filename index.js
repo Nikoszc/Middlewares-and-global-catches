09faa9b34d6eb485266949bf95b6d821e76c6439
Nikoszc <nikhilsinghg18051999@gmail.com>
2024-07-07 16:25:48 +0530

    Add existing file.

--- index.js
+++ index.js
@@ -1,62 +1,67 @@
-/**
- You need to create an express HTTP server in Node.js which will handle the logic of a file server.
-- Use built in Node.js `fs` module
-The expected API endpoints are defined below,
-1. GET /files - Returns a list of files present in `./files/` directory
-Response: 200 OK with an array of file names in JSON format.
-Example: GET http://localhost:3000/files
-2. GET /file/:filename - Returns content of given file by name
-    Description: Use the filename from the request path parameter to read the file from `./files/` directory
-    Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
-    Example: GET http://localhost:3000/file/example.txt
-- For any other route not defined in the server return 404
-Testing the server - run `npm run test-fileServer` command in terminal
-*/
+
 const express = require('express');
 const app = express();
 const port = 3000;
 
-// app.get('/health-checkup',(req,res)=>{
+///DUMB WAY OF DOING INPUT VALIDATION.
+
+// app.get('/health-checkup',function(req,res) {
 //     const username = req.headers.username;
 //     const password = req.headers.password;
 //     const kidneyId = req.query.kidneyId;
-//     if(!(username === "harkirat" || password === "pass")){
-//         if(!(kidneyId === 1 || kidneyId === 2)){
-//             //do something that is expected.
-//             res.status(400).json({
-//             "msg" : "hello your kideny is not working fine."
+//     if(username != "harkirat" || password != "pass"){
+//         res.status(400).json({
+//             "msg" : "there is something wrong with your code."
             
-//         })  
-//         } return 
-       
+//         })
+//     }
+//     if (kidneyId != 1 && kidneyId != 2){
+//         res.status(400).json({
+//             "msg" : "there is something wrong with your code."
+//         })
 //     }
 //     res.json({
-//         "msg" : "Your kidney/kidneys are fine."
+//         "msg":"Everything is fine with your kidney."
 //     })
+// });
 
-// })
-   
-app.get('/health-checkup',function(req,res) {
+
+// app.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}/`);
+// });
+
+//USING MIDDLEWARES FOR CHECKS AND AUTHENTICATIONS.
+
+function userCheckMW (req,res,next){
     const username = req.headers.username;
     const password = req.headers.password;
     const kidneyId = req.query.kidneyId;
     if(username != "harkirat" || password != "pass"){
         res.status(400).json({
-            "msg" : "there is something wrong with your code."
-            
-        })
+            msg:"There was wrong a input."
+        });
+    } else{
+        next();
     }
-    if (kidneyId != 1 && kidneyId != 2){
+};
+function kidneyCheckMW (req,res,next){
+    const username = req.headers.username;
+    const password = req.headers.password;
+    const kidneyId = req.query.kidneyId;
+    if(kidneyId != 1 && kidneyId != 2){
         res.status(400).json({
-            "msg" : "there is something wrong with your code."
-        })
+            msg:"There was wrong Kidney Inputs."
+        });
+    } else{
+        next();
     }
-    res.json({
-        "msg":"Everything is fine with your kidney."
-    })
-});
+};
 
+
+app.get('/health-check',userCheckMW,kidneyCheckMW,(req,res)=>{
+    res.send("Your kidney is healthy")
+})
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
-});
+        console.log(`Server running at http://localhost:${port}/`);
+    });
